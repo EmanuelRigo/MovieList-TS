e0046fba32ab8fb53ff844751ad401b84f881707
maira chirino <112666191+MairaCeCh@users.noreply.github.com>
2024-04-27 18:52:21 -0300

    modificando card row

--- src/components/CardRow.tsx
+++ src/components/CardRow.tsx
@@ -1,5 +1,22 @@
 import { MovieData } from "../interfaces/interfaces";
 
 export const CardRow = ({ movie }: MovieData) => {
-  return <div>{movie.titulo}</div>;
+  return (
+    <div>
+      <div className="card mt-3 todo-card text-light">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src="..." className="img-fluid rounded-start" alt="..."></img>
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title ">{movie.titulo}</h5>
+
+              <p className="card-text">{movie.año}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
